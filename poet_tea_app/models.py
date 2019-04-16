0f40ca5ccd1ba081711a9cf4a307aef341584184
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-16 09:52:57 -0400

    created model

--- poet_tea_app/models.py
+++ poet_tea_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tea(models.Model):
+    title = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+    description = models.TextField
+
+    def __str__(self):
+        return self.title
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField
+    tea = models.ForeignKey(Tea, on_delete=models.CASCADE, related_name='ingredients')
+
+    def __str__(self):
+        return self.name
