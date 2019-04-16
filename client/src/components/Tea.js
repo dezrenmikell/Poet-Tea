30f9c4729abd3c9e99e30575f9e586513dec40ee
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-16 14:17:18 -0400

    added tea and tea list

---
+++ client/src/components/Tea.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Tea extends Component {
+
+    state = {
+            tea: {},
+            ingredients: [],
+    }
+
+    componentDidMount() {
+        const teaId = this.props.match.params.id;
+        this.fetchTea(teaId)
+    }
+
+    fetchTea = async (teaId) => {
+        try {
+            const teaResponse = await axios.get(`/api/teas/${teaId}`)
+            this.setState({
+                artist: teaResponse.data,
+                ingredients: teaResponse.data.ingredients,
+            })
+        }
+        catch (error) {
+            console.log(error)
+            this.setState({error: error.message})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={this.state.tea.photo_url} alt=""/>
+                <h1>{this.state.tea.title}</h1>
+                {this.state.ingredients.map(ingredient => (
+                    <div key={ingredient.id}>
+                        <h4>{ingredient.name}</h4>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Tea;
---
+++ client/src/components/TeaList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class TeaList extends Component {
+    state = {
+        error: '',
+        teas: []
+    }
+
+    componentDidMount(){
+        this.fetchTeas();
+    }
+
+    fetchTeas = async () => {
+        try {
+            const res = await axios.get('/api/teas');
+            this.setState({teas: res.data});
+        }
+        catch (err) {
+            console.log(err)
+            this.setState({error: err.message})
+        }
+    }
+
+    render() {
+        if (this.state.error){
+            return <div>{this.state.error}</div>
+        }
+        return (
+            <div>
+                <h1>All Teas</h1>
+                {this.state.teas.map(tea => (
+                    <div key={tea.id}>
+                        <Link to={`/tea/${tea.id}`} >{tea.name}</Link>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default TeaList;
