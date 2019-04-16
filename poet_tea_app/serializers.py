a64467341981c2ddc367f4764a819a614b1b0d2f
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-16 10:59:26 -0400

    created serializers

---
+++ poet_tea_app/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Tea, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'description', 'tea')
+
+class TeaSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    class Meta:
+        model = Tea
+        fields = ('id', 'title', 'photo_url', 'description', 'ingredients')
