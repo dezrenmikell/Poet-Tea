394de55508fbd08d77e3c32e15328e57544d7365
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-16 09:43:23 -0400

    added urls file

--- poet_tea_project/urls.py
+++ poet_tea_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.FrontendAppView.as_view()), #New URL for the index route
-    path('api/v1/', include('tunr_app.urls')),
+    path('api/v1/', include('poet_tea_app.urls')),
 ]
