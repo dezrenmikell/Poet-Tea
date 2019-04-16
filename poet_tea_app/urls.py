1591d15a8620272d79f2893b754b27fe6ddd9e0b
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-16 11:52:52 -0400

    registered models

--- poet_tea_app/urls.py
+++ poet_tea_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+router = routers.DefaultRouter()
+router.register('teas', views.TeaView)
+router.register('ingredients', views.IngredientView)
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
