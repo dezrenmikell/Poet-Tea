abdbb3dd08c6b38fea2df88f9cbbfe63669d7cf3
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-19 11:11:36 -0400

    created create tea form

--- client/src/components/TeaList.js
+++ client/src/components/TeaList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import axios from 'axios';
 
 class TeaList extends Component {
     state = {
         error: '',
-        teas: []
-    }
+        teas: [],
+        tea:{
+            title: "",
+            description:"",
+            photo_url:"",
+            ingredients:[],
+        },
+        redirectToHome: false,
+        createdTea: {}
+    };
 
     componentDidMount(){
         this.fetchTeas();
@@ -23,21 +31,80 @@ class TeaList extends Component {
         }
     }
 
+    createTea = ()=>{
+        axios.post("/api/v1/teas/", {tea: this.state.tea}).then(res =>{
+            this.setState({redirectToHome: true, createdTea: res.data});
+        });
+    };
+
+    handleChange = e =>{
+        const newTea = { ...this.state.tea};
+        newTea[e.target.title] = e.target.value;
+        this.setState({tea: newTea});
+    };
+
+    handleCreation = e => {
+        e.preventDefault();
+        this.createTea();
+    };
+
+    deleteTea = () =>{
+        const teaId = this.props.match.params.teaId;
+        axios.delete(`api/v1/tea/${teaId}/`);
+        this.props.history.goBack();
+    }
+
     render() {
         if (this.state.error){
             return <div>{this.state.error}</div>
         }
+        if(this.state.redirectToHome === true){
+            return <Redirect to={`/tea/${this.state.createdTea._id}/`} />;
+        }
         return (
             <div>
                 <h1>All Teas</h1>
-                {this.state.teas.map(tea => (
+                {this.state.teas.map(tea => {
+                    return(
                     <div key={tea.id}>
-                        <Link to={`/teas/${tea.id}/`} >{tea.title}</Link>
+                        <Link to={`/teas/${tea.id}/`} key={tea.id}>{tea.title}</Link>
                         <div>
                             <img src={tea.photo_url} alt=""/>
                         </div>
                     </div>
-                ))}
+                )})}
+                <h2> Create A Tea</h2>
+                <form onSubmit={this.handleCreation}>
+                    <div>
+                        <label htmlFor="title">Tea Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={this.handleChange}
+                            value={this.state.tea.title}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <input
+                            type="text"
+                            name="description"
+                            onChange={this.handleChange}
+                            value = {this.state.tea.description}
+                        />
+
+                    </div>
+                    <div>
+                        <label htmlFor="photo_url">Picture Link</label>
+                        <input
+                            type="text"
+                            name="photo_url"
+                            onChange={this.handleChange}
+                            value={this.state.tea.photo_url}
+                        />
+                    </div>
+                    <button> Create Tea</button>
+                </form>
             </div>
         );
     }
