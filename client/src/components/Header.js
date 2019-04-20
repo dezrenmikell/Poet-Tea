4e5e662dcbd06406dde2af22319d303088e34139
dezrenmikell <dezren.mikell@yahoo.com>
2019-04-20 15:35:18 -0400

    picture in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -34,6 +34,7 @@ class Header extends Component {
           <StyledLink to="/health">Health Tips</StyledLink>
           
           <h1>Poet-Tea</h1>
+          img src={'https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/35405325_908242812695738_7036877832265400320_n.jpg?_nc_cat=110&_nc_ht=scontent-atl3-1.xx&oh=bd4acef7862e5bae9c121f2ca5ddcb9b&oe=5D3BE633'} alt="" />
           <h3>AMAZING TO TASTE, TOUCH, FEEL AND SEE..SOUNDS LIKE POETRY</h3>
         <StyledLink to="/jewlerys">Jewlery Page</StyledLink>
         </CoolNav>
